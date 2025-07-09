5f6e7660f98cdf2bdfdce15b11b57ae15b83e88e
Hudu <ahmedhudu21@gmail.com>
2025-07-09 17:19:38 +0200

    updates

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -10,7 +10,7 @@ export const protect = async (req, res, next) => {
 		try {
 			token = req.headers.authorization.split(' ')[1]; // <-- removed await
 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
-			req.user = await User.findById(decoded.id).select('-password');
+			req.user = await User.findById(decoded.id).select('');
 			return next();
 		} catch (error) {
 			return res.status(401).json({ message: 'not authorized' });
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-// create base URL
 const API = axios.create({
 	baseURL: 'http://localhost:5000/api/auth',
-});
-
-API.interceptors.request.use((req) => {
-	const token = localStorage.getItem('token');
-	if (token) req.headers['authorization'] = `Bearer ${token}`;
-
-	return req;
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	withCredentials: false,
 });
 
 export default API;
