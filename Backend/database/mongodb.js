34750ac1a67eaa898c14579dfe65dbe9b2358b20
Hudu <92208883+huduhamed@users.noreply.github.com>
2025-08-25 02:42:44 +0200

    clean setup (#23)

---
+++ Backend/database/mongodb.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+// internal imports
+import { MONGODB_URL } from '../config/env.js';
+
+// internal imports
+
+// check DB connection
+if (!MONGODB_URL) throw new Error('MONGODB_URL is not defined.');
+
+// connect db
+async function connectToDB() {
+	try {
+		await mongoose.connect(MONGODB_URL);
+		console.log('connected to database.');
+	} catch (error) {
+		console.error('error connecting to DB', error);
+
+		process.exit(1);
+	}
+}
+
+export default connectToDB;
